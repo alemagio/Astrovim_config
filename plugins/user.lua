657873b95ebb66d1f48a730aee8cfadbcc12f967
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-07-05 09:27:14 +0200

    add 3 plugins and improbe mason and null-ls config

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,26 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    "f-person/git-blame.nvim",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+  },
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
 }
