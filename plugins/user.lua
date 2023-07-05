b864738cb6eaf8309cb964da21d3b0b464feafd7
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-07-05 16:38:05 +0200

    add neotest, still not working

--- plugins/user.lua
+++ plugins/user.lua
@@ -31,4 +31,54 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
+  {
+    'rcarriga/neotest',
+    dependencies = {
+      'marilari88/neotest-vitest'
+    },
+    keys = {
+      {
+        "<leader>tNF",
+        "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>",
+        desc =
+        "Debug File"
+      },
+      {
+        "<leader>tNL",
+        "<cmd>lua require('neotest').run.run_last({strategy = 'dap'})<cr>",
+        desc =
+        "Debug Last"
+      },
+      { "<leader>tNa", "<cmd>lua require('neotest').run.attach()<cr>",                desc = "Attach" },
+      { "<leader>tNf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "File" },
+      { "<leader>tNl", "<cmd>lua require('neotest').run.run_last()<cr>",              desc = "Last" },
+      {
+        "<leader>tNn",
+        "<cmd>lua require('neotest').run.run()<cr>",
+        desc =
+        "Nearest"
+      },
+      {
+        "<leader>tNN",
+        "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+        desc =
+        "Debug Nearest"
+      },
+      { "<leader>tNo", "<cmd>lua require('neotest').output.open({ enter = true })<cr>", desc = "Output" },
+      { "<leader>tNs", "<cmd>lua require('neotest').run.stop()<cr>",                    desc = "Stop" },
+      {
+        "<leader>tNS",
+        "<cmd>lua require('neotest').summary.toggle()<cr>",
+        desc =
+        "Summary"
+      },
+    },
+    config = function()
+      require('neotest').setup({
+        adapters = {
+          require('neotest-vitest')
+        }
+      })
+    end
+  }
 }
