15ed5f59409780766de0fec489204c8dced6bdd8
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-07-21 14:37:13 +0200

    add trouble and few stuff

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -10,8 +10,9 @@ return {
     config.sources = {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.prettierd,
+      null_ls.builtins.formatting.eslint_d,
     }
-    return config -- return final config table
+    -- return config -- return final config table
   end,
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -80,5 +80,20 @@ return {
         }
       })
     end
-  }
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "princejoogie/dir-telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+  },
 }
