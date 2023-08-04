c483db0e50f387fd8911fb7ca8b780e15f7f6110
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-08-04 14:41:57 +0200

    disable tsserver as formattter to fix prettier and eslint order issue

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -7,12 +7,12 @@ return {
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettierd,
-      null_ls.builtins.formatting.eslint_d,
-    }
+    -- config.sources = {
+    -- Set a formatter
+    -- null_ls.builtins.formatting.stylua,
+    -- null_ls.builtins.formatting.prettierd,
+    -- null_ls.builtins.formatting.eslint_d,
+    -- }
     -- return config -- return final config table
   end,
 }
