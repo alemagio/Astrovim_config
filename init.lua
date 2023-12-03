98a9dc5cac804540936f123380304da8691890ab
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-12-03 14:38:41 +0100

    add biome and neogit

--- init.lua
+++ init.lua
@@ -53,6 +53,7 @@ return {
     servers = {
       -- "pyright"
       "grammarly",
+      "biome",
     },
     config = {
       grammarly = function()
@@ -62,6 +63,43 @@ return {
           init_options = {
             clientId = 'client_BaDkMgx4X19X9UxxYRCXZo',
           },
+        }
+      end,
+      biome = function()
+        return {
+default_config = {
+    cmd = { 'biome', 'lsp-proxy' },
+    filetypes = {
+      'javascript',
+      'javascriptreact',
+      'json',
+      'jsonc',
+      'typescript',
+      'typescript.tsx',
+      'typescriptreact',
+    },
+    root_dir = function(fname)
+              local util = require 'lspconfig.util'
+      return util.find_package_json_ancestor(fname)
+        or util.find_node_modules_ancestor(fname)
+        or util.find_git_ancestor(fname)
+    end,
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
+https://biomejs.dev
+
+Toolchain of the web. [Successor of Rome](https://biomejs.dev/blog/annoucing-biome).
+
+```sh
+npm install [-g] @biomejs/biome
+```
+]],
+    default_config = {
+      root_dir = [[root_pattern('package.json', 'node_modules', '.git', 'biome.json')]],
+    },
+  },
         }
       end,
     }
