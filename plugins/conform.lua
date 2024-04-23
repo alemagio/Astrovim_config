e06318c6a79f6cfbd9494bf692eabdfca0c3246c
Alessandro Magionami <alessandro.magionami@gmail.com>
2024-04-23 10:43:38 +0200

    conform

--- plugins/conform.lua
+++ plugins/conform.lua
@@ -16,10 +16,10 @@ return {
     -- Define your formatters
     formatters_by_ft = {
       lua = { "stylua" },
-      javascript = { "biome" },
+      javascript = { { "biome", "eslint_d" } },
     },
     -- Set up format-on-save
-    format_on_save = { timeout_ms = 500 },
+    format_on_save = { timeout_ms = 5000, lsp_fallback = true },
     formatters = {
       biome = {
         args = { "check", "--apply", "--stdin-file-path", "$FILENAME" },
