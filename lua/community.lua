e85e1ad3106792df34926edb859ed6a77cdb1a35
Alessandro Magionami <alessandro.magionami@gmail.com>
2025-05-06 10:19:40 +0200

    astrovim 5

---
+++ lua/community.lua
@@ -0,0 +1,26 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      flavour = "mocha",
+      integrations = {
+        rainbow_delimiters = true,
+      },
+    },
+  },
+  { import = "astrocommunity.git.diffview-nvim" },
+  { import = "astrocommunity.utility.lua-json5" },
+  { import = "astrocommunity.diagnostics.trouble-nvim", opts = { position = "right", width = 100 } },
+}
